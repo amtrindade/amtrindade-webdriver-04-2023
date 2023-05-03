85f30b5696ecc7364020a7471d62d7291a0f296a
Antonio Trindade <atrindade@umov.me>
2023-05-02 21:46:51 -0300

    Refactor calculadora

--- src/test/java/com/test/CalculadoraTest.java
+++ src/test/java/com/test/CalculadoraTest.java
@@ -1,53 +1,41 @@
 package com.test;
 
+import static com.core.DriverFactory.getDriver;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Duration;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class CalculadoraTest {
-	
-	private WebDriver driver;
+import com.core.BaseTest;
+
+public class CalculadoraTest extends BaseTest{
 	
 	@BeforeEach
 	public void before() {
-		System.setProperty("webdriver.chrome.driver", 
-				"/home/atrindade/Dev/drivers/chromedriver");	
-		driver = new ChromeDriver();
-		//wait implicity
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-		driver.get("http://antoniotrindade.com.br/treinoautomacao/desafiosoma.html");
-	}
-
-	@AfterEach
-	public void after() throws InterruptedException {				
-		driver.quit();
+		getDriver().get("http://antoniotrindade.com.br/treinoautomacao/desafiosoma.html");
 	}
 	
 	@Test
 	public void testSum() throws InterruptedException {
-		WebElement tfNumber1 = driver.findElement(By.id("number1"));
+		WebElement tfNumber1 = getDriver().findElement(By.id("number1"));
 		tfNumber1.sendKeys("45");
 		
-		WebElement tfNumber2 = driver.findElement(By.id("number2"));
+		WebElement tfNumber2 = getDriver().findElement(By.id("number2"));
 		tfNumber2.sendKeys("23");
 		
-		WebElement btnSomar = driver.findElement(By.id("somar"));
+		WebElement btnSomar = getDriver().findElement(By.id("somar"));
 		btnSomar.click();
 		
-		WebElement tfTotal = driver.findElement(By.id("total"));
+		WebElement tfTotal = getDriver().findElement(By.id("total"));
 		
 		//Espera explícita
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.textToBePresentInElementValue(tfTotal, "68"));
 				
 		assertEquals("68", tfTotal.getAttribute("value"));
